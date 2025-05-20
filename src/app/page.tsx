d038b9cc0ca86a10727feaced82800bb334d367c
tdocarmo <toni.docarmo@live.fr>
2025-05-20 19:01:30 +0200

    feat: add availability indicator to Home component for new projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,10 @@ export default function Home() {
         {/* Section d'accueil avec texte à gauche et photo à droite */}
         <div className="flex flex-col md:flex-row items-center justify-between w-full max-w-6xl gap-12 py-24">
           <div className="flex flex-col items-start gap-12 max-w-xl ml-1">
+            <div className="flex items-center gap-2 border-[0.5px] border-blue-500 rounded-2xl px-4 py-2">
+              <div className="w-2.5 h-2.5 bg-green-500 rounded-full animate-pulse"></div>
+              <span className="text-blue-500 font-medium">Disponible pour de nouveaux projets</span>
+            </div>
             <h1 className="text-4xl sm:text-5xl font-bold text-left bg-gradient-to-r from-blue-500 to-blue-700 bg-clip-text text-transparent hover:from-blue-600 hover:to-blue-800 transition-all duration-300">Développeur Full-Stack</h1>
             <p className="text-lg text-foreground/80 text-left">Passionné par le développement, je construis des applications qui répondent à des besoins précis. Mon objectif est de faire en sorte que la technologie serve un but clair.</p>
             <div className="flex gap-4 mt-4">
