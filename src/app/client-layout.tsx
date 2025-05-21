9d8d6cce9280fa28aebc2340d6831da0966c649b
tdocarmo <toni.docarmo@live.fr>
2025-05-21 23:24:51 +0200

    feat: integrate @vercel/analytics for enhanced user insights and analytics tracking in the application

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/Navbar";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { SectionProvider } from "@/context/SectionContext";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function ClientLayout({
   children,
@@ -28,6 +29,7 @@ export default function ClientLayout({
           </footer>
         </div>
         <Toaster richColors position="top-center" />
+        <Analytics />
       </SectionProvider>
     </ThemeProvider>
   );
